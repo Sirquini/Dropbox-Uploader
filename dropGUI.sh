5c969ed080fa2bfd9aad516b7330d9e2d0f8f1a2
Sirquini <sirquini2@gmail.com>
2013-11-11 19:28:40 -0500

    Initial commit

---
+++ dropGUI.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+#Check the shell
+if [ -z "$BASH_VERSION" ]; then
+    echo -e "Error: Este script requiere el shell de BASH!"
+    exit 1
+fi
+
+
+
+function upload
+{
+	echo "placeholder"
+}
+function download
+{
+	echo "placeholder"
+}
+function delete
+{
+	echo "placeholder"
+}
+function move
+{
+	echo "placeholder"
+}
+function copy
+{
+	echo "placeholder"
+}
+function mkdir
+{
+	echo "placeholder"
+}
+function list
+{
+	echo "placeholder"
+}
+function share
+{
+	echo "placeholder"
+}
+function info
+{
+	echo "placeholder"
+}
+function unlink
+{
+	echo "placeholder"
+}
